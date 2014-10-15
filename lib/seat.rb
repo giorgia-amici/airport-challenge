78d6e100fb3efd816de19ef62d08d80d32a0581c
Giorgia <giorgia1london@gmail.com>
2014-10-15 13:33:04 +0100

    working on the seat class

--- lib/seat.rb
+++ lib/seat.rb
@@ -1,29 +1,36 @@
 class Seat
 attr_accessor :number, :row, :class
 
+
 def initialize
 	@number = [*1..15] #here it goes the method and not the object to select the num of seat
 
 	@row = ['A', 'B', 'C'] #here it goes the method and not the in var to assign a row
 end
 
-def assign_number
-
+def number
+	@number.sample.to_s
+	#IF SELECTED MORE THAN THREE TIMES NOT AVAILABLE ANYMORE
 end
 
 
-def assign_row
-
-
+def row
+	@row.sample
 end
 
-def assign_class
 
-end
+
+
+# def class
+	#from 1-3 all rows is first class the rest is economic
+# end
+
+
 
 def assign_seat
-
-
+	number + " " + row
 end
+
+
 
 end
